f9d224f41e535f99acc1b982e35a8c6dc5190f82
Albert <50624043+shq-90@users.noreply.github.com>
2026-04-30 18:16:08 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "SHQ | 个人主页",
+    pageTitle: "SHQ|Research",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
