3735c8ffc8b34a73e1a8e8b0b1023b51b7fbdd57
Albert <50624043+shq-90@users.noreply.github.com>
2026-05-01 19:24:42 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -40,7 +40,13 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      title: "Explorer",
+      folderClickBehavior: "collapse",
+      filterFn: (node) => {
+        return node.name !== "content" && node.name !== "papers";
+      },
+    }),
   ],
   right: [
     Component.Graph(),
@@ -64,7 +70,13 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      title: "Explorer",
+      folderClickBehavior: "collapse",
+      filterFn: (node) => {
+        return node.name !== "content" && node.name !== "papers";
+      },
+    }),
   ],
   right: [],
 }
