b82ea129f94dd1d04d8e8e1c55f85cea8524c6ee
Albert <50624043+shq-90@users.noreply.github.com>
2026-05-03 17:08:09 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,10 +15,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      "Email": "mailto:hengqing.shen@polyu.edu.hk",
       "Google Scholar": "https://scholar.google.com.hk/citations?user=2xsnOWEAAAAJ&hl=zh-CN",
-      "GitHub": "https://github.com/shq-90",
       "ResearchGate": "https://www.researchgate.net/profile/Hengqing-Shen-2?ev=hdr_xprf",
+      "GitHub": "https://github.com/shq-90",
     },
   }),
 }
